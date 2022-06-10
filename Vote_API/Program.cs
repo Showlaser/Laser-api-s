a33d782ff99d5eff3cf090af45666e7bae3daa5f
Vincent Darwinkel <vincent.darwinkel@hotmail.nl>
2022-06-10 11:03:24 +0200

    Update

--- Vote_API/Program.cs
+++ Vote_API/Program.cs
@@ -54,7 +54,7 @@ app.Run();
 static string GetConnectionString()
 {
     // Uncomment string below when creating migrations
-    return $"database=vote;keepalive=5;server=127.0.0.1;port=3306;user id=root;password=qwerty;connectiontimeout=5";
+    //return $"database=vote;keepalive=5;server=127.0.0.1;port=3306;user id=root;password=qwerty;connectiontimeout=5";
 
     IDictionary variables = Environment.GetEnvironmentVariables();
     string? server = variables["SERVER"]?.ToString();
